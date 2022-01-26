2814a2213e8576d55e989d39f414b9c93f60653b
hienbui9999 <buiduyhien@gmail.com>
2022-01-26 11:01:38 +0700

    Adjust code

--- Sources/CasperSDKInSwift/Utils/Utils.swift
+++ Sources/CasperSDKInSwift/Utils/Utils.swift
@@ -1,34 +1,7 @@
 import Foundation
 
 class Utils {
-    public static var deployGotLittle:Bool = false
-    public static func getListDeploy()->[String] {
-        let file = "file.txt"
-        var retStr:[String] = [String]()
-        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let fileURL = dir.appendingPathComponent(file)
-            do {
-                    let text2 = try String(contentsOf: fileURL, encoding: .utf8)
-                    let elements = text2.components(separatedBy: "https://testnet.cspr.live/deploy/")
-                    var counter:Int = 0;
-                    var str = ""
-                    for element in elements {
-                        let e2 = element.components(separatedBy: "\">")
-                        if e2.count > 0 {
-                            counter += 1
-                            if counter > 1 {
-                                retStr.append(e2[0])
-                            }
-                        }
-                    }
-                return retStr
-                }
-                catch {
-                   NSLog("Error reading file :\(error)")
-                }
-        }
-        return retStr
-    }
+    
 }
 extension String {
     var isValidURL: Bool {
