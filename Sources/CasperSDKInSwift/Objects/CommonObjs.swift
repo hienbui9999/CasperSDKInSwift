5bec8837827f363ee43f689ccb8b6b1a87dd5c90
hienbui9999 <buiduyhien@gmail.com>
2022-02-16 17:04:27 +0700

    Adjust comments

--- Sources/CasperSDKInSwift/Objects/CommonObjs.swift
+++ Sources/CasperSDKInSwift/Objects/CommonObjs.swift
@@ -15,18 +15,34 @@ public enum SeigniorageAllocation {
     case Delegator(Delegator2)
 }
 
+/**
+ Enumeration represents the BlockIdentifier, often used to send as parameter when call the RPC methods
+ */
 public enum BlockIdentifier {
+    ///The block hash
     case Hash(String)
+    ///The block height
     case Height(UInt64)
+    ///Block identifier with no value of hash or height, use this None value to send parameter as nil
     case None
 }
 public class EraInfo {
     public var listSeigniorageAllocation:[SeigniorageAllocation]=[SeigniorageAllocation]();
 }
 
+/**
+ Class represents the Big number of type U512. The value is stored in String format and then parsed as number.
+ */
+ 
 public class U512Class {
-    public var realValue:[UInt64]?
+    ///Value stored in string format, example "909889988788781234567899876543211223455566"
     public var valueInStr:String=""
+    /**
+        Generate a U512 class from number in String format
+        - Parameter : number in String format
+        - Returns: U512Class object
+        */
+
     public static func fromStringToU512(from:String)->U512Class {
         let ret:U512Class = U512Class();
         ret.valueInStr = from;
@@ -34,9 +50,18 @@ public class U512Class {
     }
 }
 
+/**
+ Class represents the Big number of type U256. The value is stored in String format and then parsed as number.
+ */
+ 
 public class U256Class {
-    public var realValue:[UInt64]?
+    ///Value stored in string format, example "909889988788781234567899876543211223455566"
     public var valueInStr:String=""
+    /**
+        Generate a U256 class from number in String format
+        - Parameter : number in String format
+        - Returns: U256Class object
+        */
     public static func fromStringToU256(from:String)->U256Class {
         let ret:U256Class = U256Class();
         ret.valueInStr = from;
@@ -44,9 +69,17 @@ public class U256Class {
     }
 }
 
+/**
+ Class represents the Big number of type U256. The value is stored in String format and then parsed as number.
+ */
 public class U128Class {
-    public var realValue:[UInt64]?
+    ///Value stored in string format, example "909889988788781234567899876543211223455566"
     public var valueInStr:String=""
+    /**
+        Generate a U128 class from number in String format
+        - Parameter : number in String format
+        - Returns: U128Class object
+        */
     public static func fromStringToU128(from:String)->U128Class {
         let ret:U128Class = U128Class();
         ret.valueInStr = from;
--- Sources/CasperSDKInSwift/Objects/DeployExecutionResults.swift
+++ Sources/CasperSDKInSwift/Objects/DeployExecutionResults.swift
@@ -1,10 +1,21 @@
 import Foundation
+/**
+ Class represents the NamedArg
+ */
 
 public class NamedArg {
+    ///Name of NamedArg
     public var name:String = "";
+    ///ArgsItem in CLValue 
     public var argsItem:CLValue=CLValue();
+    /**
+       Get CLValue  from Json string
+       - Parameter : a Json String represents the CLValue object
+       - Returns: CLValue object
+       */
+
     public static func jsonToCLValue(input:[String:Any]) -> CLValue {
-        var retArg:CLValue = CLValue();
+        let retArg:CLValue = CLValue();
         if let bytes = input["bytes"] as? String {
             retArg.bytes = bytes
         }
@@ -12,174 +23,7 @@ public class NamedArg {
         retArg.parsed = CLValue.getCLValueWrapper(from: input as AnyObject, clType: retArg.cl_type)
         return retArg
     }
-    
-    public func decodeCLTypeCompound(from:CLType)->String {
-        return "";
-    }
-    public func decodeCLTypePrimitive(from:CLType)->String {
-        return "";
-    }
-    /*
-    public func encodeCLTypePrimitive(from:AnyObject)->CLType {
-        var clType : CLType = .NONE
-        //primitive type
-        if let boolCLType = from["Bool"] as? Bool {
-            return clType
-        }
-        if let u8CLType = from["U8"] as? UInt8 {
-            return .U8
-        }
-        if let u32CLType = from["U32"] as? UInt32 {
-            return .U32
-        }
-        if let u64CLType = from["U64"] as? UInt64 {
-            return .U64
-        }
-        if let u128CLType = from["U128"] as? String {
-            return .U128
-        }
-        if let u256CLType = from["U256"] as? String {
-            return .U256
-        }
-        if let u512CLType = from["U512"] as? String {
-            return .U512
-        }
-        if let stringCLType = from["String"] as? String {
-            return .String
-        }//ERROR IF MAP IS key:UINT32,VALUE:String
-        if let keyMapCLType = from["key"] as? String {
-            return .String
-        }
-        if let valueMapCLType = from["value"] as? String {
-            return .String
-        }
-        if let byteArrray = from["ByteArray"] as? UInt32 {
-            return .BytesArray(byteArrray)
-        }
-        if let keyCLType = from["Key"] as? String {
-            return .Key;
-        }
-        if let publicKeyCLType = from["PublicKey"] as? String {
-            return .PublicKey;
-        }
-        if let URefClType = from["URef"] as? String {
-            return .URef
-        }
-        return clType
-    }
-    public func stringToCLTypePrimitive(input:String)->CLType {
-        if input == "String" {
-            return .String
-        } else if input == "Bool" {
-            return .Bool
-        } else if input == "U8" {
-            return .U8
-        } else if input == "U32" {
-            return .U32
-        } else if input == "U64" {
-            return .U64
-        } else if input == "U128" {
-            return .U128
-        } else if input == "I32" {
-            return .I32
-        } else if input == "I64" {
-            return .I64
-        } else if input == "U256" {
-            return .U256
-        } else if input == "U512" {
-            return .U512
-        } else if input == "String" {
-            return .String
-        } else if input == "Key" {
-            return .Key
-        } else if input == "URef" {
-            return .URef
-        } else if input == "PublicKey" {
-            return .PublicKey
-        } 
-        return .NONE
-    }
-    public func encodeCLTypeCompound(from:AnyObject)->CLType {
-        var clType:CLType = .NONE
-        if let clType = encodeCLTypePrimitive(from: from) as? CLType {
-            switch clType {
-            case .NONE:
-                break;
-            default:
-                return clType
-                break;
-            }
-        }
-        if let listCLType = from["List"] as? String {
-            clType = stringToCLTypePrimitive(input: listCLType)
-            return .List(clType)
-        } else if let listCLType = from["List"] as? AnyObject {
-            if !(listCLType is NSNull) {
-                clType = encodeCLTypeCompound(from: listCLType)
-                return .List(clType)
-            }
-        } else {
-            
-        }
-        //FOR MAP TYPE
-        if let mapCLType = from["Map"] as? AnyObject {
-            if !(mapCLType is NSNull) {
-                if let keyCLType = encodeCLTypePrimitive(from: mapCLType) as? CLType {
-                    if let valueCLType = encodeCLTypePrimitive(from: mapCLType) as? CLType {
-                        return .Map(keyCLType, valueCLType)
-                    } else {
-                        let valueCLType = encodeCLTypeCompound(from: mapCLType) as! CLType
-                        return .Map(keyCLType, valueCLType)
-                    }
-                } else {
-                    let keyCLType = encodeCLTypeCompound(from: mapCLType) as! CLType
-                    if let valueCLType = encodeCLTypePrimitive(from: mapCLType) as? CLType {
-                        return .Map(keyCLType, valueCLType)
-                    } else {
-                        let valueCLType = encodeCLTypeCompound(from: mapCLType) as! CLType
-                        return .Map(keyCLType, valueCLType)
-                    }
-                }
-            }
-        }
-        //FOR TUPLE1 TYPE
-        if let tuple1CLType = from["Tuple1"] as? AnyObject {
-            
-        }
-        //FOR OPTION
-        if let optionCLType = from["Option"] as? String {
-            clType = stringToCLTypePrimitive(input: optionCLType)
-            return .Option(clType)
-        } else if let optionCLType = from["Option"] as? AnyObject {
-            if !(optionCLType is NSNull) {
-                clType = encodeCLTypeCompound(from: optionCLType)
-                return .Option(clType)
-            }
-        } else {
-            
-        }
-        return .NONE
-    }*/
 }
 
-public class DeployModuleByte {
-    public var moduleBytes:String?
-    public var args:[NamedArg]?
-}
-
-public class DeployStoredContractByHash {
-    public var hash:String?
-    public var entryPoint:String?
-    public var args:[NamedArg]?
-}
-
-
-
-public class DeployArgs {
-    public var clType:CLType = .NONE;
-    public var parse:String="";
-    public var bytes:String = "";
-    public var argsDesc:String = "";
-}
 
 
